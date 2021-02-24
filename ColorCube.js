13276f8f22828576a41da8ea12f59e5449f833e6
Daniel Huantes <daniel.huantesjr@gmail.com>
2021-02-24 16:47:28 -0600

    changed three window sizing

--- ColorCube.js
+++ ColorCube.js
@@ -24,7 +24,7 @@ function onDocumentMouseMove( event ) {
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);// for intensive problems you can give smaller values
+renderer.setSize(window.innerWidth * 2 / 3, window.innerHeight * 2 / 3);// for intensive problems you can give smaller values
 document.getElementById('colorcube').appendChild( renderer.domElement );
 //document.body.appendChild( renderer.domElement );//<canvas> element
 
