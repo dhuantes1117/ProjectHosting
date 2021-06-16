11ffb76d4a532f08334673fdd0a7180aa6ab6ba5
Daniel Huantes <daniel.huantesjr@gmail.com>
2021-06-16 14:51:04 -0500

    smartphone debugging

--- js/ColorCube/ColorCube.js
+++ js/ColorCube/ColorCube.js
@@ -82,10 +82,10 @@ const onMouseClick = function ( event ){
     scene.background = INTERSECTED.material.color;
 }
 const onTouchStart = function ( event ){
-    //pointer.x = event.changedTouches[0].pageX;
-    //pointer.y = event.changedTouches[0].pageY;
-    pointer.x = ( ( event.changedTouches[0].pageX - canvasBounds.left ) / ( canvasBounds.right - canvasBounds.left ) ) * 2 - 1;
-    pointer.y = - ( ( event.changedTouches[0].pageY - canvasBounds.top ) / ( canvasBounds.bottom - canvasBounds.top ) ) * 2 + 1;
+    pointer.x = event.changedTouches[0].pageX;
+    pointer.y = event.changedTouches[0].pageY;
+    //pointer.x = ( ( event.changedTouches[0].pageX - canvasBounds.left ) / ( canvasBounds.right - canvasBounds.left ) ) * 2 - 1;
+    //pointer.y = - ( ( event.changedTouches[0].pageY - canvasBounds.top ) / ( canvasBounds.bottom - canvasBounds.top ) ) * 2 + 1;
     raycaster.setFromCamera( pointer, camera );
     const intersects = raycaster.intersectObjects( group.children );
     if (intersects.length > 0) {
